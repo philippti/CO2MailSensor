83b41579db2ead15f6edf628ef4d1b0dc6425bef
philippti <philippt@dhbw-loerrach.de>
2020-11-14 21:36:19 +0100

    finished

---
+++ mail_handler.py
@@ -0,0 +1,68 @@
+import imaplib
+import smtplib
+from email.mime.text import MIMEText
+import re
+from sensor_readout import readout
+
+
+username = "getmyairquality@gmail.com"
+password = "NS\w*]Cc.(#p8?F\F;ni"
+
+def fetch_mail():
+
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap.login(username, password)
+    imap.select("Inbox")
+
+    def get_unseen_mail_ids():
+
+        check1, data1 = imap.search(None, "(UNSEEN)")
+        mail_ids = re.findall(r'\d', str(data1))
+
+        return mail_ids
+
+
+    def get_sender_addresses():
+        
+        ids = get_unseen_mail_ids()
+        sender_list = []
+
+        for i in ids:
+            check2, data2 = imap.fetch(str(i) , "(BODY[HEADER.FIELDS (FROM)])")
+            sender = str(data2)
+            addresses = (re.search('<(.*?)>', sender).group(1))
+            sender_list.append(addresses)
+        
+        sender_list = list(set(sender_list))
+        
+        return sender_list
+    return get_sender_addresses()
+
+   
+def answer():
+
+    sensor = readout()
+    
+    smtp_ssl_host = 'smtp.gmail.com'  
+    smtp_ssl_port = 465
+    sender = 'getmyairquality@gmail.com'
+
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server.login(username, password)
+
+    target = fetch_mail()
+    text = "Hello you, \n Thanks for requesting the air quality in Timos room. \n\n CO2: {} ppm  \n TVOC: {} ppb \n Temperature: {:4.2f} °C \n\n Thanks for asking and have a nice day!".format(sensor[0], sensor[1], sensor[2])
+
+    for i in enumerate(target):
+        msg = MIMEText(text)
+        msg['Subject'] = "Air quality in Timo's room"
+        msg['From'] = sender
+        msg['To'] = i[1]
+
+        server.sendmail(sender, i[1], msg.as_string())
+
+    server.quit()
+
+answer()
+
+print(readout())
---
+++ sensor_readout.py
@@ -0,0 +1,18 @@
+import time
+import board
+import busio
+import adafruit_ccs811
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ccs811 = adafruit_ccs811.CCS811(i2c)
+
+def readout():
+
+    co2 = ccs811.eco2
+    tvoc = ccs811.tvoc
+    temp = ccs811.temperature
+
+    return co2, tvoc, temp
+
+print(readout())
