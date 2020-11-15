67aea8ad5dd1f871978a3ef1a4fb90e5e1392797
philippti <philippt@dhbw-loerrach.de>
2020-11-15 14:33:37 +0100

    added comments in code

--- mail_handler.py
+++ mail_handler.py
@@ -2,39 +2,39 @@ import imaplib
 import smtplib
 from email.mime.text import MIMEText
 import re
-from sensor_readout import readout
+from sensor_readout import readout                          # import the custom module for reading the sensor data
 from sensor_readout import warning
 
 
-username = yourtrigger@mail.com
+username = yourtrigger@mail.com                             # login credentials for the used email address
 password = yourPassword
 
-def fetch_mail():
+def fetch_mail():                                           # method for getting incoming mail, extracting sender address and saving them
 
-    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")              # establishing a SSL secured connection to the imap server of google (if gmail is used)
     imap.login(username, password)
-    imap.select("Inbox")
+    imap.select("Inbox")                                    # selecting the "Inbox" to answer all incoming mails
 
-    def get_unseen_mail_ids():
+    def get_unseen_mail_ids():                               
 
-        check1, data1 = imap.search(None, "(UNSEEN)")
-        mail_ids = re.findall(r'\d', str(data1))
+        check1, data1 = imap.search(None, "(UNSEEN)")       # search for all unseen mails in the inbox
+        mail_ids = re.findall(r'\d', str(data1))            # extract the id's as a list of only numbers, hence the use of regex
 
         return mail_ids
 
 
-    def get_sender_addresses():
+    def get_sender_addresses():                     
         
-        ids = get_unseen_mail_ids()
-        sender_list = []
+        ids = get_unseen_mail_ids()                         # get the id's of all unanwered mails
+        sender_list = []                                    # empty list for saving the mail addresses of all unanswered mails
 
-        for i in ids:
-            check2, data2 = imap.fetch(str(i) , "(BODY[HEADER.FIELDS (FROM)])")
-            sender = str(data2)
-            addresses = (re.search('<(.*?)>', sender).group(1))
-            sender_list.append(addresses)
+        for i in ids:                                       # iteration over all mails to answer
+            check2, data2 = imap.fetch(str(i) , "(BODY[HEADER.FIELDS (FROM)])")     # saving the header of the mail in data2, sets the \Seen flag 
+            sender = str(data2)                                                     # save raw parsed string from imap.fetch
+            addresses = (re.search('<(.*?)>', sender).group(1))                     # extract only mail addresses using regex
+            sender_list.append(addresses)                                           # save all addresses in sender_list
         
-        sender_list = list(set(sender_list))
+        sender_list = list(set(sender_list))                                        # eliminate all multiple entries for not answering the same mail more than once
         
         return sender_list
     return get_sender_addresses()
@@ -42,19 +42,19 @@ def fetch_mail():
    
 def answer():
 
-    sensor = readout()
+    sensor = readout()                                      # get the sensor data in a tuple
     
-    smtp_ssl_host = 'smtp.gmail.com'  
+    smtp_ssl_host = 'smtp.gmail.com'                        # sending mail via smtp (if gmail is used)
     smtp_ssl_port = 465
     sender = 'getmyairquality@gmail.com'
 
-    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port) # establishing SSL secured smtp connection
     server.login(username, password)
 
-    target = fetch_mail()
+    target = fetch_mail()                                   # get the mail addresses
     text = "Hello you, \n Thanks for requesting the air quality in Timos room. \n\n CO2: {} ppm  \n TVOC: {} ppb \n Temperature: {:4.2f} °C \n\n Thanks for asking and have a nice day!".format(sensor[0], sensor[1], sensor[2])
 
-    for i in enumerate(target):
+    for i in enumerate(target):                             # answering all mail addresses in target 
         msg = MIMEText(text)
         msg['Subject'] = "Air quality in Timo's room"
         msg['From'] = sender
@@ -64,10 +64,10 @@ def answer():
 
     server.quit()
 
-answer()
+answer()                                                    # answer() serves as a kind of main function
 
-# if(warning()):
-
+# if(warning()):                                            # optional warning system if CO2 concentration in the rooms reaches a set threshold
+                                                            # omitted here, because sensor does not work reliable, avoiding a lot of spam mail
 #     smtp_ssl_host = 'smtp.gmail.com'  
 #     smtp_ssl_port = 465
 #     sender = 'yourtrigger@address.com'
--- sensor_readout.py
+++ sensor_readout.py
@@ -1,22 +1,22 @@
 import time
 import board
 import busio
-import adafruit_ccs811
+import adafruit_ccs811                      # importing the module for interacting with the breakout board and the sensor
 
 
-i2c = busio.I2C(board.SCL, board.SDA)
-ccs811 = adafruit_ccs811.CCS811(i2c)
+i2c = busio.I2C(board.SCL, board.SDA)       # initialize the I2C Bus of the Pi
+ccs811 = adafruit_ccs811.CCS811(i2c)        # initialize the sensor 
 
 def readout():
 
-    co2 = ccs811.eco2
+    co2 = ccs811.eco2                       # requesting the measurements from the sensor
     tvoc = ccs811.tvoc
-    temp = ccs811.temperature
+    temp = ccs811.temperature 
 
     return co2, tvoc, temp
 
 
-def warning():
+def warning():                              # optional warning if CO2 concentration reaches a threshold
 
     if(ccs811.eco2 > 1000):
 
